dd30df38b8731856a268d5a1b8fa472423bce974
Shatanshu Bodkhe <100084737+shatanshu3006@users.noreply.github.com>
2026-01-02 15:43:47 +0000

    tutorial 2

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-c!q%ja&%7ei@$i0#px0(kiu0!q0#jjgh952xa2_=*zjogy3d$l"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "your-production-domain.com"]
 
 
 # Application definition
